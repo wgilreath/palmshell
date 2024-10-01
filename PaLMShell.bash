6c42dc1af7d59f7c63307f87a266bf3f0713cac8
William F. Gilreath <will.gilreath@outlook.com>
2024-09-30 17:04:41 -0700

    Add files via upload
    Update to PaLMShell to use the Google Gemini AI API.

--- PaLMShell.bash
+++ PaLMShell.bash
@@ -1,16 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 #
-# Title: PaLM Shell - Pathways Language Model Shell.
+# Title: PaLM Shell - Pathways Language Model Shell. Now Google Gemini AI.
 #
 # Description: A interactive prompt-response shell that uses RESTful API calls
 #              to send prompts and receive the responses, display, and log the
-#              results.
+#              results from Google Gemini API.
 #
 # By William F. Gilreath (will@wfgilreath.xyz)
-# Version 1.11  10/22/2023
+# Version 2.0  09/30/2024
 #
-# Copyright © 2023 All Rights Reserved.
+# Copyright © 2024 All Rights Reserved.
 #
 # License: This software is subject to the terms of the GNU General Public License (GPL)
 #     version 3.0 available at the following link: http://www.gnu.org/copyleft/gpl.html.
@@ -18,6 +18,7 @@
 # You must accept the terms of the GNU General Public License (GPL) license agreement
 #     to use this software.
 #
+# Updated with new Google Gemini API 9-30-2024
 #
 
 #
@@ -26,9 +27,9 @@
 PALM_KEY="NHGUBE-PBATENGF-BA-XABJVAT-EBG13-PBQVAT" #pseudo-value
 
 #
+# Echo header about script, version, all that fun stuff.
 #
-#
-echo "PaLM Shell v1.1 (c) 2023 William F. Gilreath"
+echo "PaLM Shell v2.0 (c) 2024 William F. Gilreath"
 echo "All Rights Reserved. License is GPLv3       "
 echo
 
@@ -48,9 +49,8 @@ echo "Logging this session to: 'PaLM-$date_time.log'"
 echo
 
 #
-# main loop
+# main loop of PaLM shell script
 #
-
 while :
 do
   echo -n "PaLM>"
@@ -61,18 +61,21 @@ do
     break
   fi
 
-  echo "Prompt: '$prompt_line'" | tee -a $log_file #PaLM-$date_time.log
+  #check if input is blank line avoid empty prompt to Gemini AI
+  if [ -z "$prompt_line" ]; then
+    continue
+  fi
 
+  echo "Prompt: '$prompt_line'" | tee -a $log_file #PaLM-$date_time.log
   echo | tee -a $log_file #PaLM-$date_time.log
 
   curl \
   -s \
   -H 'Content-Type: application/json' \
-  -d '{ "prompt": { "text": '"\"$prompt_line\""' }}' \
-  "https://generativelanguage.googleapis.com/v1beta2/models/text-bison-001:generateText?key=$PALM_KEY" |  jq '.candidates | .[].output' | sed 's/\\n/\n/g' | tee -a $log_file #PaLM-$date_time.log
-
+  -d '{"contents":[{"parts":[{"text":'"\"$prompt_line\""'}]}]}' \
+  -X POST "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=$PALM_KEY" | jq '.candidates.[] .content .parts[] .text' | sed 's/\\n/\n/g' | tee -a $log_file 
+ 
   echo | tee -a $log_file ##PaLM-$date_time.log
-
 done
 
 echo "All done! Exiting...Have a nice day!" | tee -a $log_file #PaLM-$date_time.log
